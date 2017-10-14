5631b2979656cfabbf4eb925dd247ffe79684759
ww386362087 <tongwuwei@163.com>
2017-10-14 20:30:57 +0800

    路径文件
    Signed-off-by: ww386362087 <tongwuwei@163.com>

---
+++ Assets/Script/Tool/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Platform
+{
+#if UNITY_EDITOR
+    public static string STREAMING_ASSETS_PATH = Application.streamingAssetsPath;
+    public static string PERSISTENT_DATA_PATH = Application.dataPath + "/PersistentAssets";
+#elif UNITY_STANDALONE_WIN
+        public static string STREAMING_ASSETS_PATH = Application.streamingAssetsPath;
+        public static string PERSISTENT_DATA_PATH = Application.dataPath + "/PersistentAssets";
+#elif UNITY_IPHONE
+        public static string STREAMING_ASSETS_PATH = Application.streamingAssetsPath;
+        public static string PERSISTENT_DATA_PATH = Application.persistentDataPath;
+#elif UNITY_ANDROID
+        public static string STREAMING_ASSETS_PATH = Application.streamingAssetsPath;
+        public static string PERSISTENT_DATA_PATH = Application.persistentDataPath;
+#endif
+}
